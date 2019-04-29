7346d6ae2d4a48253a23d8a0c77c0f7d2d68babd
Hung Phan <phandinhhungvp2001@gmail.com>
2019-04-29 02:45:53 +0000

    settings: Switch styles [2/3]

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -37,6 +37,7 @@ import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
+import com.android.settings.display.SwitchStylePreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
@@ -153,6 +154,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(mUIStylePreference = new DarkUIPreferenceController(context, lifecycle, fragment));
         controllers.add(new ForceAllowThemePreferenceController(context, mUIStylePreference, mAccentPickerPreference, mQsTileStylesPreference));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new SwitchStylePreferenceController(context));
         controllers.add(new ColorModePreferenceController(context));
         return controllers;
     }
