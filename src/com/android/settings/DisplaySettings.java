e547d41e5a14e49f2c676227b57ebf3022d972ac
Hung Phan <phandinhhungvp2001@gmail.com>
2019-05-01 15:11:40 +0000

    Revert "Add QS header styles [2/3]"
    This reverts commit f3cde6237805bdb9a37a44065a9ce057237422e2.

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -42,7 +42,6 @@ import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.DarkUIPreferenceController;
-import com.android.settings.display.QsHeaderStylePreferenceController;
 import com.android.settings.display.QsTileStylesPreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
@@ -153,7 +152,6 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ForceAllowThemePreferenceController(context, mUIStylePreference, mAccentPickerPreference));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(new SwitchStylePreferenceController(context));
-        controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new ColorModePreferenceController(context));
         return controllers;
     }
